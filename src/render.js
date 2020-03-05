1307187303bdf81558e21cabfeac574b00aebed4
stefaleon <stefaleon@gmail.com>
2020-03-05 22:59:18 +0200

    DOM elements

---
+++ src/render.js
@@ -0,0 +1,4 @@
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
